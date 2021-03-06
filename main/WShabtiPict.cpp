c5dd5498b56ae78e9ff6d5260db3674ca5985737
br0tda <peppedellecave@live.it>
2021-03-06 13:10:20 +0100

    Added possibility to use Test/Oracle/JUnit generator modules from terminal

--- main/WShabtiPict.cpp
+++ main/WShabtiPict.cpp
@@ -14,18 +14,35 @@
 
 #define PARAMETER_FILE "./tmp/model.txt"
 #define TEST_CASE_FILE "./tmp/test_case.csv"
+#define TEST_CASE_MODE "-t"
+#define ORACLE_GENERATION_MODE "-o"
+#define JUNIT_TEST_CLASS_GENERATION_MODE "-j"
+
+#define MODE argv[1]
+#define SOURCE argv[2]
+#define PARAMETER argv[3]
+#define INPUT_FILE argv[4]
+#define K_DEGREE argv[5]
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+    bool is_interactive = true;
+    string source_code_path = "";
 
+    if(argc != 1)
+    {
+        is_interactive = false;
+    }
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 
-    //TAKE SOURCECODE PATH
-    cout << "SourceCode Path: ";
-    string source_code_path = "";
-    cin >> source_code_path;
+    if(is_interactive)
+    {
+        //TAKE SOURCECODE PATH
+        cout << "SourceCode Path: ";
+        cin >> source_code_path;
+    
 
     //TAKE PROTOTYPE FORM SOURCECODE
     ofstream parameter_file;
@@ -196,8 +213,92 @@ int main()
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 
     //CONCLUSION
-    cout<<endl<<"Your tests have been generated, please check JUnitTests/ "<<endl;
-    return 0;
+    cout<<endl<<"Your tests have been generated, please check JUnitTests/"+class_name+"/"<<endl;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
+    }
+
+    else
+    {
+        string mode = "";
+
+        mode = MODE;
+        source_code_path = SOURCE;
+
+        //TAKE PROTOTYPE FORM SOURCECODE
+        vector<string> all_prototypes = get_prototypes(source_code_path);
+        string prototype;
+
+        //TAKE THE METHOD TO TEST
+        prototype = all_prototypes[stoi(PARAMETER)-1];
+
+        //GET CLASS NAME
+        string class_name = get_class_name(source_code_path);
+
+        if(mode.compare(TEST_CASE_MODE) == 0)
+        {
+            string parameter_input_file_path = "";
+
+            //CREATE DIRECTORY FOR CONTAINING FINAL FILES (.java class and .csv files)
+            filesystem::create_directory("./JUnitTests/"+class_name);
+ 
+            parameter_input_file_path = INPUT_FILE;
+
+            //---------COPY INPUT FILE IN THE OUTPUT DIRECTORY---------// 
+                ifstream source(parameter_input_file_path, ios::binary);
+                ofstream dest(PARAMETER_FILE, ios::binary);
+
+                istreambuf_iterator<char> begin_source(source);
+                istreambuf_iterator<char> end_source;
+                ostreambuf_iterator<char> begin_dest(dest);
+
+                copy(begin_source, end_source, begin_dest);
+
+                source.close();
+                dest.close();
+            //---------COPY INPUT FILE IN THE OUTPUT DIRECTORY---------//
+
+            test_generator(stoi(K_DEGREE), PARAMETER_FILE, TEST_CASE_FILE);
+            cout<<"Your tests have been generated, please check tmp/test_case.csv"<<endl;
+        }
+        
+        else if(mode.compare(ORACLE_GENERATION_MODE) == 0)
+        {
+            vector<string> method_and_values = get_method_and_values_name(prototype);
+            string method_name = method_and_values[0];
+        
+            //GET ALL POSSIBILE ORACLES
+            ifstream test_case_input_file;
+            
+            test_case_input_file.open(INPUT_FILE, std::ios_base::in);
+            vector<string> oracle;
+            string test_case = "";
+            string test_case_oracle = "";
+
+            cout<<"\n-----ORACLE: "<<prototype<<"-----"<<endl;
+            cout<<"Please specify the oracle for the following possible combinations"<<endl;
+            cout<<"For a range of numbers you can use the syntax \"expected_value:delta\""<<endl;
+
+            while(!test_case_input_file.eof())
+            {
+                getline(test_case_input_file, test_case);
+                cout<<method_name<<"("<<test_case<<"): ";
+                cin>>test_case_oracle;
+                oracle.push_back(test_case_oracle);
+            }
+
+            append_oracle_to_csv(oracle, TEST_CASE_FILE,"./JUnitTests/"+class_name+"/"+method_name+"_test.csv");
+            cout<<endl<<"Your CSV with Oracles has been generated, please check JUnitTests/"+class_name+"/"+method_name+"_test.csv"<<endl;
+        }
+    
+        else if(mode.compare(JUNIT_TEST_CLASS_GENERATION_MODE) == 0)
+        {
+            vector<string> prototype_tested;
+            prototype_tested.push_back(prototype);
+            test_source_code_generate(source_code_path, "./JUnitTests/"+class_name+"/", class_name, prototype_tested);
+            cout<<"Your test has been generated, please check JUnitTests/"+class_name+"/"<<endl;
+        }
+    }
+    
+    return 0;
 }
